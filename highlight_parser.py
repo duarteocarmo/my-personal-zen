69af695a4bcec522cb145985001fbd86c523d741
Rakesh Sarangi <rakesh.sarangi@meesho.com>
2021-12-05 20:26:14 +0530

    Indent notes to the parent highlight and remove duplicate highlights

--- highlight_parser.py
+++ highlight_parser.py
@@ -7,9 +7,7 @@ import pathlib
 import datetime
 
 current_directory = pathlib.Path.cwd()
-parsed_books = list(
-    set(file.stem for file in current_directory.glob("**/*.md"))
-)
+parsed_books = list()
 highlight_separator = "=========="
 highlight_json = dict()
 library = []
@@ -22,12 +20,12 @@ class Book:
     def __init__(self, title, author):
         self.author = author
         self.title = title
-        self.highlights = []
+        self.highlights = set()
         Book.book_list.add(self.title)
 
     def add_highlight(self, highlight):
         if highlight:
-            self.highlights.append(highlight)
+            self.highlights.add(highlight)
 
     def __str__(self):
         return f"<Book Object>Title:{self.title}\tAuthor:{self.author}\tHighlights:{len(self.highlights)}"
@@ -43,8 +41,11 @@ class Book:
             file.write(f"# {clean_title}")
             file.write("\n")
             for h in self.highlights:
-                clean_text = h.replace("\n", " ")
-                file.write(f"- {clean_text}")
+                clean_text = h.content.replace("\n", " ")
+                if (h.isNote):
+                    file.write(f"\t- NOTE: {clean_text}")
+                else:
+                    file.write(f"\n- {clean_text}")
                 file.write("\n")
 
             file.close()
@@ -58,6 +59,7 @@ class Highlight:
             self.title,
             self.author,
             self.content,
+            self.isNote,
         ) = Highlight.parse_single_highlight(raw_string)
 
     def __str__(self):
@@ -68,29 +70,35 @@ class Highlight:
         splitted_string = list(filter(None, highlight_string.split("\n")))
 
         if len(splitted_string) != 3:
-            return None, None, None
+            return None, None, None, None
 
         # first parse
         author_line = splitted_string[0]
         content = splitted_string[-1]
         info_line = splitted_string[-2]
+        info_line_parts = info_line.split("|")
 
         # parse author and title
-        regex = r"\((.*?)\)"
         match = re.search("\((.*)\)", author_line)
 
         if not match:
-            return None, None, None
+            return None, None, None, None
 
         author = match.group(1)
         title = author_line[: match.start()]
 
+
         # parse date
         if ADD_DATES:
-            date = info_line.split("|")[-1][10:]
+            date = info_line_parts[-1][10:]
             content = f"(Added on {date}) {content}"
 
-        return title, author, content
+        isNoteMatch = re.search(r"(\w+) (\w+)", info_line_parts[0])
+        if not isNoteMatch:
+            return None, None, None, None
+        isNote = (isNoteMatch.group(2) == "Note")
+
+        return title, author, content, isNote
 
 
 with open("My Clippings.txt", "r") as file:
@@ -102,12 +110,12 @@ for raw_string in highlights:
     h = Highlight(raw_string)
     if h.title not in Book.book_list:
         b = Book(h.title, h.author)
-        b.add_highlight(h.content)
+        b.add_highlight(h)
         library.append(b)
     else:
         for b in library:
             if b.title == h.title:
-                b.add_highlight(h.content)
+                b.add_highlight(h)
 
 for book in library:
     if book.title:
